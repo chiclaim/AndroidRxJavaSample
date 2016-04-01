4d7a9e76ab38abd74755125bde08c94c70287bef
chiclaim <chiclaim@gmail.com>
2016-04-01 14:43:41 +0800

    update README.txt

--- app/src/main/java/com/chiclaim/rxjava/UseRxJavaRightWayActivity.java
+++ app/src/main/java/com/chiclaim/rxjava/UseRxJavaRightWayActivity.java
@@ -50,6 +50,7 @@ public class UseRxJavaRightWayActivity extends AppCompatActivity {
                 });
 
         compositeSubscription.add(subscriptionForUser);
+
     }
 
     public static void launch(Context context) {
